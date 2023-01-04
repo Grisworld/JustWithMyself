83066a7f7fb40cb3ef371f26bd2b06e43b58c7ee
Grisworld <potemkin11>
2023-01-04 10:59:39 +0300

    Worked_so_hard for tablayout..

---
+++ app/src/main/java/com/example/basicattempt/AcademicCalendar.java
@@ -0,0 +1,54 @@
+package com.example.basicattempt;
+
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
+
+public class AcademicCalendar extends AppCompatActivity {
+
+
+    private TabLayout tabLayout;
+    private ViewPager2 viewPager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_academic_calendar);
+        tabLayout = findViewById(R.id.tabs);
+        viewPager = findViewById(R.id.view_pager);
+        VPAdapter vpAdapter = new VPAdapter(this);
+        viewPager.setAdapter(vpAdapter);
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+        viewPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                tabLayout.getTabAt(position).select();
+            }
+        });
+
+
+
+        //setContentView(binding.getRoot());
+
+
+    }
+}
