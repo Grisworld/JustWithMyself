7152d59fca6f94e18e0c382999df7e164de78756
Grisworld <potemkin11>
2023-01-04 16:52:56 +0300

    Still working...

--- app/src/main/java/com/example/basicattempt/AcademicCalendar.java
+++ app/src/main/java/com/example/basicattempt/AcademicCalendar.java
@@ -1,18 +1,22 @@
 package com.example.basicattempt;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
+import com.example.basicattempt.ui.main.HomeFragment;
 import com.google.android.material.tabs.TabLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager2.widget.ViewPager2;
+
 
 public class AcademicCalendar extends AppCompatActivity {
 
 
     private TabLayout tabLayout;
     private ViewPager2 viewPager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,13 +24,21 @@ public class AcademicCalendar extends AppCompatActivity {
         tabLayout = findViewById(R.id.tabs);
         viewPager = findViewById(R.id.view_pager);
         VPAdapter vpAdapter = new VPAdapter(this);
-
+        System.out.println("OBJECT "+this.getClass());
         viewPager.setAdapter(vpAdapter);
+        /*
+        //findViewById((R.layout.activity_academic_calendar).spinner);
 
+        getSupportFragmentManager().beginTransaction()
+                .setReorderingAllowed(true)
+                .add(R.id.spinner, HomeFragment.class, savedInstanceState)
+                .commit();
+*/
         tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
             public void onTabSelected(TabLayout.Tab tab) {
                 viewPager.setCurrentItem(tab.getPosition());
+
             }
 
             @Override
