9c9cc16feb44bc8744eaa4fe7278d298388cbd62
Grisworld <potemkin11>
2023-01-05 18:43:04 +0300

    Working On MapActivity

---
+++ app/src/main/java/com/example/basicattempt/TwoMaps.java
@@ -0,0 +1,14 @@
+package com.example.basicattempt;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TwoMaps extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_two_maps);
+    }
+}
