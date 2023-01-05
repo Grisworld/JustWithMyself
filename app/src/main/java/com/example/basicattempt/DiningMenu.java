507cdb8ad8337664c265223641151f2d5227c406
Grisworld <potemkin11>
2023-01-05 17:28:18 +0300

    DiningMenu added

---
+++ app/src/main/java/com/example/basicattempt/DiningMenu.java
@@ -0,0 +1,14 @@
+package com.example.basicattempt;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DiningMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dining_menu);
+    }
+}
